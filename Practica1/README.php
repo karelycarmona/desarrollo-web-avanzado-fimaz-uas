926a5af48fef5e1386cedead9937160dd62272e8
karelycarmona <karelycarmonamedina@gmail.com>
2026-03-06 23:53:44 -0600

    README.php

--- Practica1/README.php
+++ Practica1/README.php
@@ -1,1 +1,26 @@
+# Práctica 1 - Creación de clases y encapsulamiento en PHP
 
+# Objetivo
+Aplicar los fundamentos de programación orientada a objetos en PHP mediante la creación de una clase con atributos privados, constructor y métodos de acceso.
+
+# Descripción
+Se creó una clase llamada Usuario con dos atributos privados:
+- nombre
+- correo
+
+La clase incluye:
+- Constructor
+- Métodos getters
+- Métodos setters
+
+# Instrucciones de ejecución
+
+1. Colocar la carpeta en el servidor local (XAMPP).
+2. Iniciar Apache.
+3. Abrir el navegador.
+4. Acceder a:
+
+http://192.168.1.20/desarrollo-web
+
+# Resultado esperado
+Mostrar en pantalla el nombre y correo del usuario utilizando los métodos getters.
