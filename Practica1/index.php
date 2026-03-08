f4fd7bdea3192973ac69738f55ec9525ed039e71
karelycarmona <karelycarmonamedina@gmail.com>
2026-03-07 20:34:00 -0600

    index.php

--- Practica1/index.php
+++ Practica1/index.php
@@ -2,7 +2,7 @@
 
 require_once "Usuario.php"
   
-$usuario1 = new Usuario("Karely", "karely@gmail.com");
+$usuario = new Usuario("Karely", "karely@gmail.com");
 
 echo "Nombre: " . $usuario1->getNombre() . "<br>";
 echo "Correo: " . $usuario1->getCorreo();
