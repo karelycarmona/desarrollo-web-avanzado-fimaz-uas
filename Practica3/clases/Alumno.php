63506f4be56a7cb2fa9c069fe42ceac759c8beec
karelycarmona <karelycarmonamedina@gmail.com>
2026-03-08 20:23:02 -0600

    Alumno.php

---
+++ Practica3/clases/Alumno.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once "Usuario.php";
+
+class Alumno extends Usuario {
+
+    private $matricula;
+
+    public function __construct($nombre, $correo, $matricula){
+
+        parent::__construct($nombre, $correo);
+
+        if(empty($matricula)){
+            throw new Exception("La matrícula es obligatoria");
+        }
+
+        $this->matricula = $matricula;
+    }
+
+    public function getRol(){
+        return "Alumno";
+    }
+
+    public function getMatricula(){
+        return $this->matricula;
+    }
+
+}
