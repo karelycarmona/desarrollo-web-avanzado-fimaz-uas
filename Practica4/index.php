a45bfdaee350ca7efd134cc5db05855333602e24
karelycarmona <karelycarmonamedina@gmail.com>
2026-03-08 23:42:57 -0600

    Update index.php

--- Practica4/index.php
+++ Practica4/index.php
@@ -1,40 +1,49 @@
 <?php
 
-require_once 'clases/Admin.php';
-require_once 'clases/Alumno.php';
-require_once 'clases/Invitado.php';
+require_once "clases/Admin.php";
+require_once "clases/Alumno.php";
+require_once "clases/Invitado.php";
 
 try{
 
-    echo "<h2>ADMIN</h2>";
+$admin = new Admin("Karely","karely@correo.com",7);
+$alumno = new Alumno("Juan","juan@correo.com","A001");
+$invitado = new Invitado("Luis","luis@correo.com","1234");
 
-    $admin = new Admin("Karely","karely@correo.com",3);
+echo "<table border='1'>";
 
-    echo $admin->getNombre()."<br>";
-    echo $admin->getEmail()."<br>";
-    echo "Nivel: ".$admin->getNivel()."<br>";
+echo "<tr>
+<th>Tipo</th>
+<th>Nombre</th>
+<th>Email</th>
+<th>Extra</th>
+</tr>";
 
-    echo "<hr>";
+echo "<tr>
+<td>Admin</td>
+<td>".$admin->getNombre()."</td>
+<td>".$admin->getEmail()."</td>
+<td>Nivel ".$admin->getNivel()."</td>
+</tr>";
 
-    echo "<h2>ALUMNO</h2>";
+echo "<tr>
+<td>Alumno</td>
+<td>".$alumno->getNombre()."</td>
+<td>".$alumno->getEmail()."</td>
+<td>".$alumno->getMatricula()."</td>
+</tr>";
 
-    $alumno = new Alumno("Juan","juan@correo.com","A001");
+echo "<tr>
+<td>Invitado</td>
+<td>".$invitado->getNombre()."</td>
+<td>".$invitado->getEmail()."</td>
+<td>".$invitado->getCodigo()."</td>
+</tr>";
 
-    echo $alumno->getNombre()."<br>";
-    echo $alumno->getEmail()."<br>";
-    echo "Matricula: ".$alumno->getMatricula()."<br>";
-
-    echo "<hr>";
-
-    echo "<h2>INVITADO</h2>";
-
-    $invitado = new Invitado("Luis","luis@correo.com","1234");
-
-    echo $invitado->getNombre()."<br>";
-    echo $invitado->getEmail()."<br>";
-    echo "Codigo: ".$invitado->getCodigo()."<br>";
+echo "</table>";
 
 }catch(Exception $e){
 
-    echo "Error: ".$e->getMessage();
+echo "Error: ".$e->getMessage();
+
 }
